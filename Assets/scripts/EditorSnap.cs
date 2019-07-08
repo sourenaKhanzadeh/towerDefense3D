411839d77514169132738151aa4ccd21184dbb1c
Sourena Khanzadeh <soure@DESKTOP-6PVNFEF.(none)>
2019-07-07 21:28:17 -0400

    grid system created

---
+++ Assets/scripts/EditorSnap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+    [SerializeField] float gridSize = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 snapPos = new Vector3(0,0,0);
+        Vector3 scale = new Vector3(1f, 1f, 1f) * gridSize;
+
+        transform.localScale = scale;
+
+        snapPos.x = Mathf.RoundToInt(transform.position.x / 
+            gridSize) * gridSize;
+
+        snapPos.z = Mathf.RoundToInt(transform.position.z/gridSize)*gridSize;
+
+        transform.position = snapPos;
+    }
+}
