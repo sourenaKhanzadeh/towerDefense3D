466fac503458a2b7b91652fe7d701e366432dbd1
Sourena Khanzadeh <soure@DESKTOP-6PVNFEF.(none)>
2019-07-07 22:52:38 -0400

    world created

--- Assets/scripts/EditorSnap.cs
+++ Assets/scripts/EditorSnap.cs
@@ -13,12 +13,19 @@ public class EditorSnap : MonoBehaviour
         Vector3 snapPos = new Vector3(0,0,0);
         Vector3 scale = new Vector3(1f, 1f, 1f) * gridSize;
 
+
         transform.localScale = scale;
 
         snapPos.x = Mathf.RoundToInt(transform.position.x / 
             gridSize) * gridSize;
 
         snapPos.z = Mathf.RoundToInt(transform.position.z/gridSize)*gridSize;
+
+        string coord_text = snapPos.z / gridSize + "," + snapPos.x / gridSize;
+
+        TextMesh coordinate = GetComponentInChildren<TextMesh>();
+        coordinate.text = coord_text;
+        gameObject.name = coord_text;
 
         transform.position = snapPos;
     }
