fed464c6ea4c85764b3ac609ffa32cdb83e0ec72
Sourena Khanzadeh <soure@DESKTOP-6PVNFEF.(none)>
2019-07-07 23:14:24 -0400

    enemy movement created

---
+++ Assets/scripts/EnemyMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] List<Block> path;
+    [SerializeField] float delay = 1f;
+
+
+    [SerializeField] Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = new Vector3(0f, transform.position.y, 0f);
+        StartCoroutine(PrintAllWaypoints());
+    }
+
+    IEnumerator PrintAllWaypoints() {
+        foreach (Block waypoint in path)
+        {
+            transform.position = waypoint.transform.position + offset;
+            yield return new WaitForSeconds(delay);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
